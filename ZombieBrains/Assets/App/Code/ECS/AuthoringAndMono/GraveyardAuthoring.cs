95f6ad8f2f725245c9a8cdf5ab5a77e954263f7a
PawelCzebreszuk <czebreszuk.pawel@gmail.com>
2024-11-22 13:22:40 +0100

    Add basic setup for graveyard

---
+++ ZombieBrains/Assets/App/Code/ECS/AuthoringAndMono/GraveyardAuthoring.cs
@@ -0,0 +1,42 @@
+﻿using ECS.ComponentsAndTags;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace ECS.AuthoringAndMono
+{
+	public class GraveyardAuthoring : MonoBehaviour
+	{
+		#region SerializeFields
+
+		[SerializeField] private float2 m_fieldDimensions;
+		[SerializeField] private int m_numberTombstonesToSpawn;
+		[SerializeField] private GameObject m_tombstonePrefab;
+
+		#endregion
+
+		#region NestedTypes
+
+		public class GraveyardPropertiesBaker : Baker<GraveyardAuthoring>
+		{
+			#region PublicMethods
+
+			public override void Bake(GraveyardAuthoring authoring)
+			{
+				AddComponent(
+					GetEntity(TransformUsageFlags.None),
+					new GraveyardProperties
+					{
+						FieldDimensions = authoring.m_fieldDimensions,
+						NumberTombstonesToSpawn = authoring.m_numberTombstonesToSpawn,
+						TombstonePrefab = GetEntity(authoring.m_tombstonePrefab, TransformUsageFlags.Renderable)
+					}
+				);
+			}
+
+			#endregion
+		}
+
+		#endregion
+	}
+}
---
+++ ZombieBrains/Assets/App/Code/ECS/ComponentsAndTags/GraveyardProperties.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace ECS.ComponentsAndTags
+{
+	public struct GraveyardProperties : IComponentData
+	{
+		#region PublicFields
+
+		public float2 FieldDimensions;
+		public int NumberTombstonesToSpawn;
+		public Entity TombstonePrefab;
+
+		#endregion
+	}
+}
